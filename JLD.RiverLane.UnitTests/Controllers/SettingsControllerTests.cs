cdfc9ad996026effdad2e23d9bb80c48473b5455
Jim Light <jimlight@jimlightdevelopment.co.uk>
2016-10-18 18:00:51 +0100

    added some tests

---
+++ JLD.RiverLane.UnitTests/Controllers/SettingsControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using JLD.RiverLane.Controllers;
+using System;
+using Xunit;
+
+namespace JLD.RiverLane.UnitTests.Controllers
+{
+    public class SettingsControllerTests
+    {
+        [Fact]
+        public void Constructor_ServiceIsNull_Throws()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new SettingsController(null));
+        }
+    }
+}
---
+++ JLD.RiverLane.UnitTests/Services/Settings/SettingsServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using JLD.RiverLane.Services.Settings;
+using Moq;
+using System;
+using Xunit;
+
+namespace JLD.RiverLane.UnitTests.Services.Settings
+{
+    public class UsersServiceTests
+    {
+        private readonly ISettingsService dummyEdit;
+
+        public UsersServiceTests()
+        {
+            dummyEdit = new Mock<ISettingsService>().Object;
+        }
+
+        [Fact]
+        public void Constructor_EditIsNull_Throws()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new SettingsService(null));
+        }
+    }
+}
